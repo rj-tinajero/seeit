854811c6d3a7ea67fde704a961fb9f6e1dd68b9b
Richard Tinajero <rj.tinajero92@gmail.com>
2019-03-11 18:23:41 -0700

    checkpoint fin

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,7 +2,10 @@ module.exports = {
     init(app) {
         const staticRoutes = require("../routes/static");
         const aboutRoutes = require("../routes/about");
+        const topicRoutes = require("../routes/topics");
+        
         app.use(staticRoutes);
         app.use(aboutRoutes);
+        app.use(topicRoutes);
     }
 }
