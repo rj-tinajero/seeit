de8d333ff3be803e5440ea5010244571ee9aa669
Richard Tinajero <rj.tinajero92@gmail.com>
2019-03-20 14:34:40 -0700

    cp9 done

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,10 +1,12 @@
 module.exports = {
     init(app) {
         const staticRoutes = require("../routes/static");
+        const postRoutes = require("../routes/posts");
         const aboutRoutes = require("../routes/about");
         const topicRoutes = require("../routes/topics");
         
         app.use(staticRoutes);
+        app.use(postRoutes);
         app.use(aboutRoutes);
         app.use(topicRoutes);
     }
