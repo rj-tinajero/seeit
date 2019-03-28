df24108731740aa9c414b5811da2bce862fb2728
Richard Tinajero <rj.tinajero92@gmail.com>
2019-03-28 10:59:00 -0700

    delete function help

--- src/routes/flairs.js
+++ src/routes/flairs.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const flairController = require("../controllers/flairController");
 
 router.get("/topics/:topicId/posts/:postId/new", flairController.new);
-router.post("/topics/:topicId/posts/:postId/create", flairController.create);
+router.post("/topicsFlair/:topicId/posts/:postId/create", flairController.create);
 router.get("/topics/:topicId/posts/:id", flairController.show);
 router.post("/topicsFlair/:topicId/posts/:postId/destroy", flairController.destroy);
 
