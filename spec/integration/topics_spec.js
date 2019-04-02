ca1c226669cedc3b3761b5974723ab3e5ac5cae4
Richard Tinajero <rj.tinajero92@gmail.com>
2019-04-02 10:51:53 -0700

    assign done

--- spec/integration/topics_spec.js
+++ spec/integration/topics_spec.js
@@ -73,6 +73,30 @@ describe("routes : topics", () => {
             }
           );
         });
+
+     it("should not create a new topic that fails validations", (done) => {
+       const options = {
+         url: `${base}/create`,
+         form: {
+           title: "a",
+           description: "b"
+         }
+       };
+       request.post(options,
+         (err, res, body) => {
+           Topic.findOne({where: {title: "a"}})
+           .then((topic) => {
+             console.log(topic);
+               expect(topic).toBeNull();
+               done();
+           })
+           .catch((err) => {
+             console.log(err);
+             done();
+           });
+         }
+       );
+     });
       });
       describe("GET /topics/:id", () => {
           it("should render a view with the selected topic", (done) => {
