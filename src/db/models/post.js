438c019fc99f08cd24de75c5a148eda5ca5e74ed
Richard Tinajero <rj.tinajero92@gmail.com>
2019-04-03 13:38:46 -0700

    autho update

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -12,12 +12,20 @@ module.exports = (sequelize, DataTypes) => {
     topicId: {
       type: DataTypes.INTEGER,
       allowNull: false
-    }
+    },
+    userID: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
   }, {});
   Post.associate = function(models) {
     // associations can be defined here
     Post.belongsTo(models.Topic, {
       foreignKey: "topicId",
+      onDelete: "CASCADE"
+    });
+    Post.belongsTo(models.User, {
+      foreignKey: "userId",
       onDelete: "CASCADE"
     });
     Post.hasMany(models.Flair, {
